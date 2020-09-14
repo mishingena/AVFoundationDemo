3ee74a5416c327a80415273ba075dd43a06c5f69
Gennadiy Mishin <mishingena@gmail.com>
2020-09-15 00:05:44 +0300

    Initial Commit

---
+++ AVFoundationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundationDemo
+//
+//  Created by Gennadiy Mishin on 15.09.2020.
+//  Copyright © 2020 GM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
